e9cdcde2d4d4e366d1c1c1784f14b5914923403f
kafawi <15199037+kafawi@users.noreply.github.com>
2021-12-21 15:08:32 +0100

    tidy up solution

--- SOLUTIONS/p025/solution.py
+++ SOLUTIONS/p025/solution.py
@@ -1,34 +1,14 @@
-from collections import namedtuple
-from typing import List, Tuple
-
-# iteration 2
+# iteration 2 and 3
 def matches(text :str, pattern :str) -> bool: 
-    #states = pattern_2_state_seq(pattern)
     return _matches(text, 0, pattern, 0) 
 
-def pattern_2_state_seq(pattern :str) -> List["_State"] :
-  states = []
-  _State = namedtuple("State", "char quantifyer")
-  skip =False
-  for i in range(len(pattern) -1,-1,-1):
-    if skip:
-        skip = False 
-        continue   
-    if pattern[i] == '*':
-        state = _State(char = pattern[i-1], quantifyer = '*')
-        skip = True
-    else:
-      state = _State(char = pattern[i], quantifyer = None)
-    states.insert(0, state)
-  return states
-
-
 def _matches(text :str, idx_text_start :int, pattern :str , idx_pattern: int) -> bool:
+    # sourcery skip: merge-else-if-into-elif, reintroduce-else
     for i in range(idx_text_start, len(text)):
         # guard if text is > pattern
         if not _is_idx_in_bounds(pattern, idx_pattern): return False
         if _is_optional(pattern, idx_pattern):
-            # skip
+            # skip this state and try the next with recurrsion (this is greedy)
             if _matches(text, i, pattern, idx_pattern + 2): return True
             # else check if char matches
             if not _matches_char(pattern[idx_pattern], text[i]): return False
@@ -54,7 +34,6 @@ def _is_idx_in_bounds(array :str, idx :int) ->bool:
 def _matches_char(pattern_char, text_char :str) -> bool:
     return "." in pattern_char or text_char in pattern_char
     
-
 """
 # iteration 0
 def matches(text :str, pattern :str) -> bool:

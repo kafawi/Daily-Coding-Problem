eaecac8091e6be8255c49b56cfef3acc4e0eeb66
kafawi <15199037+kafawi@users.noreply.github.com>
2022-01-13 16:19:08 +0100

    Make The Answer faster with binary search

--- SOLUTIONS/p042/concept.md
+++ SOLUTIONS/p042/concept.md
@@ -36,4 +36,37 @@ find_subset_for_sum(S' :[int], k :int)-> [int]
   return null
 ```
 
+### optimazation
+
+To search for the "first" element `S[i]` that is `S[i] <= k`, we could use binary search.
+
+Befor that search, we can check if there are any by check first the minimum of S.
+
+```pseudo
+find_subset_for_sum(S' :[int], k :int)-> [int]
+  if S' is empy or S'[0] > k: return null
+  i = find_rightmost_leq(arr,k)
+  if S'[i] == k: return S'[i]
+
+  for j in i...0:
+    subS = find_subset_for_sum(S'[:j], k - S[j])
+    if subS != null: return subS + S'[0]
+  return null
+
+find_rightmost_leq(arr :[int], T :int) -> index:
+  # optional short cuts
+  if arr[last] <= T : return last
+  if arr[first] > T : return -1
+  
+  l = first = 0
+  r = last = arr.length -1
+  while(l <= r):
+    m = floor of (l+r) / 2
+    if arr[m] <= T: l = m + 1
+    if arr[m] > T: r = m - 1
+  return r
+```
+
+To safe the passing overhead. let us just pass an index for the upper bound, because the sorted List is no more manipulated.
+
 [code](solution.py)
--- SOLUTIONS/p042/solution.py
+++ SOLUTIONS/p042/solution.py
@@ -2,16 +2,16 @@
 
     >>> sorted(find_subset_for_sum([12, 1, 61, 5, 9, 2], 24))
     [1, 2, 9, 12]
-    
+
     >>> find_subset_for_sum([12, 1, 61, 5, 9, 2], 1)
     [1]
-    
+
     >>> find_subset_for_sum([12, 1, 61, 5, 9, 2], 61)
     [61]
-    
+
     >>> find_subset_for_sum([12, 11, 10, 14], 1) == None
     True
-    
+
     >>> sorted(find_subset_for_sum([], 1))
     Traceback (most recent call last):
     AssertionError
@@ -21,24 +21,37 @@
 
 def find_subset_for_sum(numbers: list[int], sum_k: int) -> list[int] | None:
     assert numbers and min(numbers) > 0
-    sorted_S = sorted(numbers)
-    return _find_subset_for_sum(sorted_S, sum_k)
+    S = sorted(numbers)
 
+    def _find_subset_for_sum(upper_bound: int, k: int) -> [int]:
+        if upper_bound == 0 or S[0] > k:
+            return None
 
-def _find_subset_for_sum(S: list[int], k: int) -> list[int] | None:
-    if not S:
+        i = _find_rightmost_leq(S, upper_bound, k)
+        if S[i] == k:
+            return [S[i]]
+
+        for j in range(i, -1, -1):
+            subS = _find_subset_for_sum(j, k - S[j])
+            if subS:
+                return [S[j]] + subS
         return None
-    i = len(S)-1
-    while(i > 0 and S[i] > k):
-        i -= 1
-    if S[i] == k:
-        return [k]
 
-    for j in range(i, -1, -1):
-        subS = _find_subset_for_sum(S[:j], k - S[j])
-        if subS:
-            return [S[j]] + subS
-    return None
+    return _find_subset_for_sum(len(S), sum_k)
+
+
+def _find_rightmost_leq(arr, upper_bound, T) -> int:
+    r = upper_bound - 1
+    if arr[r] <= T:
+        return r
+    l = 0
+    while(l <= r):
+        m = (l+r) // 2
+        if arr[m] <= T:
+            l = m + 1
+        else:  # if arr[m] > T:
+            r = m - 1
+    return r
 
 
 if __name__ == '__main__':
